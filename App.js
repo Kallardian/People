49c5d552cd1a2cd543c705aa5a30f0e1ffb6f4f1
Kallardian <paulobertanha03@gmail.com>
2021-05-31 08:35:01 -0300

    Renderizando listas na tela

--- App.js
+++ App.js
@@ -3,10 +3,27 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/Header";
 
-export default function App() {
-  return (
-    <View>
-      <Header title="Pessoas" />
-    </View>
-  );
+// renderList(){
+//   const names = ["Eddie Van Halen", "Jimi Hendrix", "Chimbinha", "Steve Val"];
+// }
+
+export default class App extends React.Component {
+  renderList() {
+    const names = ["Eddie Van Halen", "Jimi Hendrix", "Chimbinha", "Steve Val"];
+
+    const textNames = names.map((name) => {
+      return <Text>{name}</Text>;
+    });
+
+    return textNames;
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas" />
+        {this.renderList()}
+      </View>
+    );
+  }
 }
