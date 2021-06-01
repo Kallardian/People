353cfe744528c0b3594e5655f88932f256014b5d
Kallardian <paulobertanha03@gmail.com>
2021-06-01 08:52:58 -0300

    Tranformando renderList() em um component

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 import Header from "./src/components/Header";
+import PeopleList from "./src/components/PeopleList";
+
 import axios from "axios";
 /*
   Vai chamar o render()
@@ -31,20 +34,11 @@ export default class App extends React.Component {
       });
   }
 
-  renderList() {
-    const textElements = this.state.peoples.map((people) => {
-      const { first } = people.name;
-      return <Text key={first}>{first}</Text>;
-    });
-
-    return textElements;
-  }
-
   render() {
     return (
       <View>
         <Header title="Pessoas" />
-        {this.renderList()}
+        <PeopleList peoples={this.state.peoples} />
       </View>
     );
   }
