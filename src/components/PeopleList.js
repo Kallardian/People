97d2cc33aedd5b55b153fc65954f1588b9ca193d
Kallardian <paulobertanha03@gmail.com>
2021-06-01 15:19:38 -0300

    Estilizando PoepleList com FlexBox

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,15 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const PeopleList = (props) => {
   const { peoples } = props;
 
   const textElements = peoples.map((people) => {
     const { first } = people.name;
-    return <Text key={first}>{first}</Text>;
+    return (
+      <View key={first} style={styles.line}>
+        <Text style={styles.lineText}>{first}</Text>
+      </View>
+    );
   });
 
-  return <View>{textElements}</View>;
+  return <View style={styles.container}>{textElements}</View>;
 };
 
 export default PeopleList;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#e2f9ff",
+  },
+  line: {
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: "#bbb",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  lineText: {
+    fontSize: 20,
+    paddingLeft: 15,
+  },
+});
