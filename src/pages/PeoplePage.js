fa297591e0bcbd1038b5752e20e0f20fdc744fa5
Kallardian <paulobertanha03@gmail.com>
2021-06-01 20:37:08 -0300

    Criando a pasta Pages para diferentes 'screens'

---
+++ src/pages/PeoplePage.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+import Header from "../components/Header";
+import PeopleList from "../components/PeopleList";
+
+import axios from "axios";
+/*
+  Vai chamar o render()
+  Depois que renderizar, ele chama o componentDidMount()
+  E aí dentro do CDM a gente coloca o Axios
+  E aí mudamos o state, que vai fazer renderizar de novo
+  E agora com as pessoas já salvas no State
+*/
+
+export default class PeoplePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      peoples: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=15")
+      .then((response) => {
+        const { results } = response.data; // destruct, ele vai procurar um campo results dentro de response.data
+        this.setState({
+          //overwriting o state inicial
+          peoples: results,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas" />
+        <PeopleList peoples={this.state.peoples} />
+      </View>
+    );
+  }
+}
