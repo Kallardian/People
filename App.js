6af111a0c5f114de29a08b8a683388cd61c65e41
Kallardian <paulobertanha03@gmail.com>
2021-06-01 08:41:25 -0300

    Usando  componentDidMount

--- App.js
+++ App.js
@@ -2,29 +2,42 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/Header";
 import axios from "axios";
-
-// renderList(){
-//   const names = ["Eddie Van Halen", "Jimi Hendrix", "Chimbinha", "Steve Val"];
-// }
+/*
+  Vai chamar o render()
+  Depois que renderizar, ele chama o componentDidMount()
+  E aí dentro do CDM a gente coloca o Axios
+  E aí mudamos o state, que vai fazer renderizar de novo
+  E agora com as pessoas já salvas no State
+*/
 
 export default class App extends React.Component {
-  renderList() {
-    // const textNames = names.map((name) => {
-    //   return <Text key={name}>{name}</Text>; //aqui no key, eu tenho que ter certeza que NÃO vai existir outra chave igual
-    // });
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      peoples: [],
+    };
+  }
+
+  componentDidMount() {
     axios
       .get("https://randomuser.me/api/?nat=br&results=5")
       .then((response) => {
         const { results } = response.data; // destruct, ele vai procurar um campo results dentro de response.data
-        const names = results.map((people) => {
-          //fez um map pelo results pra pegar "people" e retornou o primeiro nome
-          return people.name.first;
+        this.setState({
+          //overwriting o state inicial
+          peoples: results,
         });
-        console.log(names);
       });
+  }
 
-    // return textNames;
+  renderList() {
+    const textElements = this.state.peoples.map((people) => {
+      const { first } = people.name;
+      return <Text key={first}>{first}</Text>;
+    });
+
+    return textElements;
   }
 
   render() {
