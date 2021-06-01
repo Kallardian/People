8e2efa3e0b27c75cad9a1e3f71cf167a1d360b46
Kallardian <paulobertanha03@gmail.com>
2021-06-01 08:12:41 -0300

    Puxando apenas os nomes da API

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/Header";
+import axios from "axios";
 
 // renderList(){
 //   const names = ["Eddie Van Halen", "Jimi Hendrix", "Chimbinha", "Steve Val"];
@@ -9,13 +9,22 @@ import Header from "./src/components/Header";
 
 export default class App extends React.Component {
   renderList() {
-    const names = ["Eddie Van Halen", "Jimi Hendrix", "Chimbinha", "Steve Val"];
+    // const textNames = names.map((name) => {
+    //   return <Text key={name}>{name}</Text>; //aqui no key, eu tenho que ter certeza que NÃO vai existir outra chave igual
+    // });
 
-    const textNames = names.map((name) => {
-      return <Text key={name}>{name}</Text>; //aqui no key, eu tenho que ter certeza que NÃO vai existir outra chave igual
-    });
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=5")
+      .then((response) => {
+        const { results } = response.data; // destruct, ele vai procurar um campo results dentro de response.data
+        const names = results.map((people) => {
+          //fez um map pelo results pra pegar "people" e retornou o primeiro nome
+          return people.name.first;
+        });
+        console.log(names);
+      });
 
-    return textNames;
+    // return textNames;
   }
 
   render() {
