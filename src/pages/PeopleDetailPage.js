2f0a63c10523907cb91d5878b7f403ef88e98c68
Kallardian <paulobertanha03@gmail.com>
2021-06-02 08:42:23 -0300

    Adicionando imagem ao PeopleDetail

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 
 export default class PeopleDetailPage extends React.Component {
   render() {
+    const { people } = this.props.navigation.state.params;
+
     return (
-      <View>
+      <View style={styles.container}>
+        <Image style={styles.avatar} source={{ uri: people.picture.large }} />
         <Text>Eu sou a PeopleDetailPage</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  avatar: {
+    aspectRatio: 1,
+    borderRadius: 200,
+  },
+});
