f6c54b7ab1113c610f0b97b38c1fd39a53ed6442
Kallardian <paulobertanha03@gmail.com>
2021-06-01 21:12:59 -0300

    Apagando header antigo

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-import Header from "../components/Header";
 import PeopleList from "../components/PeopleList";
 
 import axios from "axios";
@@ -37,7 +36,6 @@ export default class PeoplePage extends React.Component {
   render() {
     return (
       <View>
-        <Header title="Pessoas" />
         <PeopleList peoples={this.state.peoples} />
       </View>
     );
