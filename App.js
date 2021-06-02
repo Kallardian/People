9149a30bbb9303f363d81035bafeeb16b45771b9
Kallardian <paulobertanha03@gmail.com>
2021-06-01 21:12:49 -0300

    Adicionando lib Navigation-Stack

--- App.js
+++ App.js
@@ -1,3 +1,32 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
 import PeoplePage from "./src/pages/PeoplePage";
 
-export default PeoplePage;
+const AppNavigator = createStackNavigator(
+  {
+    Main: {
+      screen: PeoplePage,
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      title: "Pessoas",
+      headerStyle: {
+        backgroundColor: "#3c8be6",
+        borderBottomWidth: 2,
+        borderBottomColor: "#c0c0c0",
+      },
+      headerTitleStyle: {
+        color: "white",
+        fontSize: 30,
+        flexGrow: 1, //ocupe todo espaço que você puder
+        textAlign: "center",
+      },
+    },
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
