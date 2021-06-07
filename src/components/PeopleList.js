7d88d0d0768cf6cfd336ea9cefad0bc7b3bea892
Kallardian <paulobertanha03@gmail.com>
2021-06-07 07:48:03 -0300

    Usando FlatList para Scroll

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,24 +1,21 @@
 import React from "react";
-import { ScrollView, Text, StyleSheet } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 
 import PeopleListItem from "./PeopleListItem.js";
 
 const PeopleList = (props) => {
   const { peoples, onPressItem } = props;
-  const items = peoples.map((people) => {
-    const uniqueKey = people.name.first + people.name.last;
-    return (
-      <PeopleListItem
-        key={uniqueKey}
-        people={people}
-        navigateToPeopleDetail={onPressItem}
-      />
-    );
-  });
-
-  return <ScrollView style={styles.container}>{items}</ScrollView>;
+  return (
+    <FlatList
+      style={styles.container}
+      data={peoples}
+      renderItem={({ item }) => (
+        <PeopleListItem people={item} navigateToPeopleDetail={onPressItem} />
+      )}
+      keyExtractor={(item) => item.login.username}
+    />
+  );
 };
-
 export default PeopleList;
 
 const styles = StyleSheet.create({
